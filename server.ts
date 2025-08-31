b15f3b475a03b7ea1bdeb651df9a5ce02dce0e57
imchirag22 <im.chirag.20@gmail.com>
2025-08-31 13:43:13 +0530

    express setup for mobile app in js

--- server.ts
+++
@@ -1,9 +0,0 @@
-import app from "./src/app.js";
-
-const startServer = (): void => {
-    const port = process.env.PORT || 3000
-    app.listen(port)
-    console.log(`Server is running on ${port}`)
-}
-
-startServer()
