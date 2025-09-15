bfb997489d45afdfa0360bcb082dd48d37f5e8c5
imchirag22 <im.chirag.20@gmail.com>
2025-09-16 01:43:00 +0530

    delete and userRecommedation routes

--- backend/src/controllers/bookController.js
+++ backend/src/controllers/bookController.js
@@ -18,8 +18,8 @@ const getBooks = asyncHandler(async (req,res) => {
         const newBook = await Book.create({
             title,
             captions,
-            image: imageUrl,
-            rating,
+            images: imageUrl,
+            ratings: rating,
             user: req.user._id
         })
         res.status(201).json(newBook)
@@ -28,7 +28,7 @@ const getBooks = asyncHandler(async (req,res) => {
         res.status(401).json({message: error.message})
     }
 })
-const getAllBooks = asyncHandler(async(req,res)=> {
+const getAllBooks = asyncHandler(async(req,res) => {
     try {
         const page = req.query.page || 1
         const limit = req.query.limit || 5
@@ -52,5 +52,42 @@ const getAllBooks = asyncHandler(async(req,res)=> {
         res.status(500).json({message: "error loading the books", error})
     }
 })
+const deleteBook = asyncHandler(async ( req,res ) => {
+   try {
+    const book = await Book.findById(req.params.id)
+    if (!book) {
+        return res.status(401).json({message: "Book not found"})
+    }
 
-export {getBooks , getAllBooks}
+    //checking if the one deleting is the creator of the post
+    if (book.user.toString() !== req.user._id.toString())
+        return res.status(401).json({message: "Unauthorized"})
+
+    // deleting image from cloudinary 
+    if (book.images && book.images.includes("cloudinary")) {
+        try {
+          const publicId = book.images.split("/").pop().split(".")[0];
+          await cloudinary.uploader.destroy(publicId)
+        } catch (deleteError) {
+          console.log("Error deleting image from cloudinary", deleteError);
+        }
+      }
+  
+      await book.deleteOne()
+      res.status(200).json({message: "Book deleted successfully"})
+      
+   } catch (error) {
+       res.status(500).json({message: "Error deleting book", error: error.message})
+   }
+})
+const userRecommendation = asyncHandler( async (req, res) => {
+    try {
+      const books = await Book.find({ user: req.user._id }).sort({ createdAt: -1 });
+      res.json(books);
+    } catch (error) {
+      console.error("Get user books error:", error.message);
+      res.status(500).json({ message: "Server error" });
+    }
+  })
+
+export {getBooks , getAllBooks, deleteBook, userRecommendation}
--- backend/src/routes/booksRoutes.js
+++ backend/src/routes/booksRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import  {  getBooks , getAllBooks } from "../controllers/bookController";
+import  {  getBooks , getAllBooks, deleteBook, userRecommendation } from "../controllers/bookController";
 import { verifyJWT } from "../middleware/authMiddleware";
 
 const router = express.Router()
 
 router.post('/',verifyJWT, getBooks)
 router.get('/',verifyJWT, getAllBooks)
+router.delete('/:id',verifyJWT, deleteBook)
+router.get("/user", verifyJWT, userRecommendation);
 
 export default router
