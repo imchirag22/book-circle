c59939ab04c9c3c70fb561bdf6650a088fb694cd
imchirag22 <im.chirag.20@gmail.com>
2025-09-17 16:21:15 +0530

    layout setup of auth & index and safeareascreen
    EXPO 54 UPGRADE

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import SafeAreaScreen from "../components/SafeAreaScreen"
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+    <SafeAreaScreen>
+  <Stack screenOptions={{headerShown:false}}>
+    <Stack.Screen name="index" />
+    <Stack.Screen name="(auth)" />
+  </Stack>
+  </SafeAreaScreen>
+  <StatusBar style="dark" />
+  </SafeAreaProvider>
+  )
 }
--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
 
-export default function Index() {
+const index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>index</Text>
+      <Link href="(auth)/">Login Screen</Link>
+      <Link href="(auth)/signup">Sign Up Screen</Link>
     </View>
-  );
+  )
 }
+
+export default index
